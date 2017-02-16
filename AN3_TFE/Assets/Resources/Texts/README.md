8e6e05f852151181fbfff8d88131714126f78a28
AYMERIC-MSI\Aymeric <xhin_12@hotmail.fr>
2017-02-16 18:03:19 +0100

    reworking dialog system

---
+++ AN3_TFE/Assets/Resources/Texts/README.md
@@ -0,0 +1,10 @@
+File name must have this template :
+
+"sceneID"_"npcID"_"questStep"_"depth"+"choice[equalto:depth]"-"buttonsCount(0 is endDialog)"
+
+sceneID : Match with the ID of the scene (specified in the QuestManager)
+npcID : Match with the Id of the npc the player's talking to (specified in the NpcManager attached to the npc)
+questStep : Match with the actual step of the quest of this npc (specified in the QuestManager)
+depth : The depth of the choices tree
+choice : 0=left choice; 1=right choice; range of numbers must be equal to [depth] (example: if depth = 1, file name's depth+choice must have the format "00")
+buttonsCount : number of choices after the dialog (0 is the last dialog and close the text box)
--- AN3_TFE/Assets/Script/DialoguesSystem.cs
+++ AN3_TFE/Assets/Script/DialoguesSystem.cs
@@ -24,7 +24,12 @@ public class DialoguesSystem : MonoBehaviour
     public static bool mousePressed;
    // QuestManager qManager;
     CharacterClickingController controller;
-    public int choicesCount, sceneID, npcID, step, order;
+    public int
+        sceneID,
+        npcID,
+        step,
+        order,
+        choicesCount;
 
     void Awake()
     {
@@ -45,14 +50,9 @@ public class DialoguesSystem : MonoBehaviour
                 if (mousePressed && currentLine >= endAtLine)
                 {
                     mousePressed = false;
-                    if (endDialog)
-                    {
-                        currentLine = 0;
-                        textBox.SetActive(false);
-                        isTextboxActive = false;
-                    }
                     else
                     {
+                        theText.enabled = false;
                         for (int i = 0; i < choicesCount; i++)
                         {
                             buttons[i].SetActive(true);
@@ -68,8 +68,14 @@ public class DialoguesSystem : MonoBehaviour
         }
     }
 
-    void ResumeDialog()
+    public void ResumeDialog()
     {
+        if (endDialog)
+        {
+            currentLine = 0;
+            textBox.SetActive(false);
+            isTextboxActive = false;
+        } else
         mousePressed = true;
     }
 
@@ -85,8 +91,20 @@ public class DialoguesSystem : MonoBehaviour
         npcID = _npcID;
         step = _step;
         textBox.SetActive(true);
+        textBox.GetComponent<Button>().interactable = true;
         order = 0;
-        textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order) as TextAsset;
+        textFile = null;
+        //textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order + 0) as TextAsset;
+        for (int i = 0; i < 3; i++) {
+            textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order + "-" + i) as TextAsset;
+            if (textFile != null)
+                break;
+        }
+        string[] fileName;
+        fileName = textFile.name.Split('-');
+        choicesCount = int.Parse(fileName[1]);
+        if (choicesCount == 0)
+            endDialog = true;
         if (textFile != null)
             textLines = (textFile.text.Split('\n'));
         if (endAtLine == 0)
@@ -97,18 +115,36 @@ public class DialoguesSystem : MonoBehaviour
 
 
     //A REFAIRE, TROUVER UN MOYEN DE LOAD TEXT DANS L'ORDRE
-    void choiceOne()
+    public void nextDialog(int choice)
     {
-        textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order + "0") as TextAsset;
-    }
-
-    void choiceTwo()
-    {
-        textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order + "1") as TextAsset;
-    }
-
-    void choiceThree()
-    {
-        textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order + "00") as TextAsset;
+        order += 1;
+        string strRange = "";
+        for (int i = 0; i < order; i++)
+            strRange += "0";
+        string choiceString = choice.ToString(strRange);
+        textFile = null;
+        for (int i = 0; i < 3; i++)
+        {
+            textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order + choiceString + "-" + i) as TextAsset;
+            if (textFile != null)
+                break;
+        }
+        //textFile = Resources.Load("Texts/" + sceneID + "_" + npcID + "_" + step + "_" + order + choice + anyChar) as TextAsset;
+        if (textFile != null)
+            textLines = (textFile.text.Split('\n'));
+        if (endAtLine == 0)
+            endAtLine = textLines.Length - 1;
+        currentLine = 0;
+        theText.enabled = true;       
+        for (int i = 0; i < choicesCount; i++)
+        {
+            print(choicesCount);
+            buttons[i].SetActive(false);
+        }
+        string[] fileName;
+        fileName = textFile.name.Split('-');
+        choicesCount = int.Parse(fileName[1]);
+        if (choicesCount == 0)
+            endDialog = true;
     }
 }
